799059ed939d5a12edf639de54c8efa4a1d6fb3c
yaojun <yaojun0415bean@sina.com>
2017-03-18 15:27:40 +0800

    deno

--- vaporDemo/Sources/App/main.swift
+++ vaporDemo/Sources/App/main.swift
@@ -1,11 +1,19 @@
 import Vapor
+import VaporMySQL
 
 let drop = Droplet()
+let driver = try MySQLDriver.init(host: "localhost", user: "root", password: "", database: "yaojun")
+
 
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
+}
+
+drop.get("users") { req in
+    let result = try driver.mysql("select nickname,uid,username from users;")
+    return try JSON(node: result[0])
 }
 
 drop.resource("posts", PostController())
